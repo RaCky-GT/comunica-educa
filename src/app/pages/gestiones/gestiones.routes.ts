467c90cf1866f0dcd56f93df26f364f6aecddc2e
@RaCky-GT <rguarchajt2@gmail.com>
2023-08-18 01:02:26 -0600

    Delete Pages

--- src/app/pages/gestiones/gestiones.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from "@angular/router";
-import { AddGestionComponent } from "./add-gestion/add-gestion.component";
-import { ListGestionesComponent } from "./list-gestiones/list-gestiones.component";
-
-export const GestionesRoutes: Routes = [
-    {path: '', title: 'Lista Gestiones', component: ListGestionesComponent},
-    {path: 'add', title: 'Nueva Gestion', component: AddGestionComponent},
-    {path: 'edit', title: 'Editar Gestion', component: AddGestionComponent},
-];

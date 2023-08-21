242bbd9cfb701a25df50f86b216d29d03669a56a
@RaCky-GT <rguarchajt2@gmail.com>
2023-08-21 00:22:30 -0600

    add home page and not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+  {
+    path: '',
+    loadComponent: () => import('@shared/pages').then(c => c.HomePageComponent)
+  },
+  {
+    path: 'not-found',
+    loadComponent: () => import('@shared/pages').then(c => c.NotFoundPageComponent)
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found',
+  }
+
+
 ];
 
 @NgModule({
---
+++ src/app/shared/pages/not-found-page/not-found-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-not-found-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './not-found-page.component.html',
+  styles: [
+  ]
+})
+export class NotFoundPageComponent {
+
+}

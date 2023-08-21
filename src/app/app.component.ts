fbe8c668c0edcb68a7185f1675d6f0590191bc36
@RaCky-GT <rguarchajt2@gmail.com>
2023-08-21 17:10:51 -0600

    add prime ng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject} from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet/>',
 })
-export class AppComponent {
-  isCollapsed = false;
+export class AppComponent implements OnInit{
+
+  private primeNgConfig = inject(PrimeNGConfig);
+
+  ngOnInit(): void {
+    this.primeNgConfig.ripple = true;
+  }
 }
---
+++ src/app/modules/prime-ng/prime-ng.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { RippleModule } from 'primeng/ripple';
+import { ButtonModule } from 'primeng/button';
+
+
+
+@NgModule({
+  exports: [
+    RippleModule,
+    ButtonModule
+  ]
+})
+export class PrimeNgModule { }

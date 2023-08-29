c871ef47b89b304f7cec3b2e642acbd7abd7a829
@RaCky-GT <rguarchajt2@gmail.com>
2023-08-28 18:44:22 -0600

    base complete routes

--- src/app/base/base-routing.module.ts
+++ src/app/base/base-routing.module.ts
@@ -5,7 +5,13 @@ import {BaseComponent} from "@base/base.component";
 const routes: Routes = [
   {
     path: '',
-    component: BaseComponent
+    component: BaseComponent,
+    children: [
+      { path: 'managements', loadChildren: () => import('@base/managements/managements.module').then(m => m.ManagementsModule) },
+      { path: 'professors', loadChildren: () => import('@base/professors/professors.module').then(m => m.ProfessorsModule) },
+      { path: 'students', loadChildren: () => import('@base/students/students.module').then(m => m.StudentsModule) },
+      { path: '**', redirectTo: 'managements', pathMatch: 'full' },
+    ]
   }
 ];
 
---
+++ src/app/base/managements/managements.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-managements',
+  templateUrl: './managements.component.html',
+  styles: [
+  ]
+})
+export class ManagementsComponent {
+
+}

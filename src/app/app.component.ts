f6a93718a3fdcc3de02570ba6f10385cb35b99a0
@RaCky-GT <rguarchajt2@gmail.com>
2023-08-30 23:17:26 -0600

    add tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet/>',
+  template: '<router-outlet/> <h1 class="text-3xl font-bold underline">Hello world!</h1>',
 })
 export class AppComponent {
   isCollapsed = false;

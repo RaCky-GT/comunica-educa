557aec866d269cf744cf065d6bed36e6c6c1ecb3
@RaCky-GT <rguarchajt2@gmail.com>
2023-09-07 12:19:33 -0600

    Add fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +19,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'comunica-educa',
+    appId: '1:208069313892:web:9a39c48d0d3644bf136671',
+    storageBucket: 'comunica-educa.appspot.com',
+    apiKey: 'AIzaSyAGe3SDR__h0Dyx1KQUuZ8arUdVp14wUp8',
+    authDomain: 'comunica-educa.firebaseapp.com',
+    messagingSenderId: '208069313892',
+  },
+};

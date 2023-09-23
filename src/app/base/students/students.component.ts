0d29a71987e3be1dd99bd56a0a283a601ffe1d7b
@RaCky-GT <rguarchajt2@gmail.com>
2023-09-23 15:11:08 -0600

    add students in public view

--- src/app/base/students/students.component.ts
+++ src/app/base/students/students.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { Students } from '@core/models';
+
+import { StudentsService } from '@core/services';
 
 @Component({
   selector: 'app-students',
@@ -6,6 +9,34 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class StudentsComponent {
+export class StudentsComponent implements OnInit{
+  private studentsService = inject(StudentsService);
+  public students = signal<Students[] | undefined>(undefined);
+  public studentsWasFound = signal(true);
 
+  ngOnInit(): void {
+    this.loadStudents();
+  }
+
+  loadStudents() {
+    this.studentsService.getStudents().subscribe({
+      next: (resp) => {
+        this.students.set(resp);
+        if (resp.length > 0) {
+          this.studentsWasFound.set(true);
+        } else {
+          this.studentsWasFound.set(false);
+        }
+      },
+      error: (error) => {
+        console.log(error);
+        this.students.set(undefined);
+        this.studentsWasFound.set(false);
+      },
+    });
+  }
+
+  openNewTab(url:string) {
+    window.open(url, '_blank');
+  }
 }

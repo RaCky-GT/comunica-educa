5bf3e90c3ea8bf1ff4d29ec26a5620351a3acc97
@RaCky-GT <rguarchajt2@gmail.com>
2023-09-23 15:17:16 -0600

    add vista de recursos de catedraticos

--- src/app/base/professors/professors.component.ts
+++ src/app/base/professors/professors.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+
+import { ProfessorsService } from '@core/services';
+import { Professors } from '@core/models';
 
 @Component({
   selector: 'app-professors',
@@ -6,6 +9,34 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class ProfessorsComponent {
+export class ProfessorsComponent implements OnInit{
+  private professorService = inject(ProfessorsService);
+  public professors = signal<Professors[] | undefined>(undefined);
+  public professorsWasFound = signal(true);
 
+  ngOnInit(): void {
+    this.loadProfessors();
+  }
+
+  loadProfessors() {
+    this.professorService.getProfessors().subscribe({
+      next: (resp) => {
+        this.professors.set(resp);
+        if (resp.length > 0) {
+          this.professorsWasFound.set(true);
+        } else {
+          this.professorsWasFound.set(false);
+        }
+      },
+      error: (error) => {
+        console.log(error);
+        this.professors.set(undefined);
+        this.professorsWasFound.set(false);
+      },
+    });
+  }
+
+  openNewTab(url:string) {
+    window.open(url, '_blank');
+  }
 }
